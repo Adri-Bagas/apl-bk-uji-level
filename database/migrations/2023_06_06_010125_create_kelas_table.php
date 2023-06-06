f612c18709419a604e806c1ed727a6928cccbde0
Adri-Bagas <abagaswitjaksono@gmail.com>
2023-06-06 08:18:00 +0700

    first commit

---
+++ database/migrations/2023_06_06_010125_create_kelas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kelas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->foreignId('walas_id')->constrained('gurus', 'id');
+            $table->foreignId('bk_id')->constrained('gurus', 'id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kelas');
+    }
+};
---
+++ database/migrations/2023_06_06_011140_add_column_kelas_id_to_siswas.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('siswas', function (Blueprint $table) {
+            $table->foreignId('kelas_id')->constrained('kelas', 'id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('siswas', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('kelas_id');
+        });
+    }
+};
