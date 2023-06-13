b0c529225539405ab29fa723759fe9a8e05148c0
Adri-Bagas <abagaswitjaksono@gmail.com>
2023-06-13 12:57:19 +0700

    fix tampilan andy

--- resources/views/policy.blade.php
+++ resources/views/policy.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="pt-4 bg-gray-100">
-        <div class=" items-center pt-6 sm:pt-0">
+        <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
             <div>
                 <x-authentication-card-logo />
             </div>
